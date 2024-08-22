753f327eadaa36f66ab92316ade610a1590132c4
Jose Aldair U. Hdz. <josealdairugaldehernandez@gmail.com>
2024-08-21 18:10:44 -0600

    cambio para las rutas

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Contacto from '../views/ContactoView.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('#'),
   routes: [
     {
       path: '/',
